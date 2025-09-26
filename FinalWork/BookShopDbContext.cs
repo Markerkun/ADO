607560dd0ee3b5e29c7f1f2c0021c011ad38b9a2
mka <mka@A21PC16>
2025-09-26 17:57:31 +0300

    classwork

--- FinalWork/BookShopDbContext.cs
+++ FinalWork/BookShopDbContext.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FinalWork.Entities;
 
 namespace FinalWork
 {
-    internal class BookShopDbContext
+    internal class BookShopDbContext : DbContext
     {
+        public BookShopDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;
+                                        Initial Catalog = MusicalShop;
+                                        Integrated Security=True;
+                                        Connect Timeout=5;
+                                        Encrypt=False;Trust Server Certificate=False;
+                                        Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+        public DbSet<Author> Authors { get; set; }
     }
 }
